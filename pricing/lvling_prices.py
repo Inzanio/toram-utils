099ef90bac37738d6deaa48a3f102f774ebf83b4
LoonerDrive <noa.edouardmichel@gmail.com>
2025-04-06 09:35:45 +0100

    allow user to play with my cote

--- pricing/lvling_prices.py
+++ pricing/lvling_prices.py
@@ -10,12 +10,12 @@ cote=286
 pricing_unit = 100000
 col1,col2,col3 = st.columns(3)
 col1.metric("🔝 Level", f"{300}",help=f"Current :blue[level max] in Toram")
-col2.metric("My cote", f"⚖️{286}",label_visibility="visible", help=f"A unit to calculate :green[price] base on :blue[exp] needed, More it :red[increase] more the :green[price] is :red[high].\nIt will :red[increase] with game :red[inflation]")
 
 col_start_lvl , col_end_lvl = st.columns(2)
 start_level = col_start_lvl.number_input("Enter the level you will :green[start] with", min_value=1, max_value=level_cap-1)
 end_level = col_end_lvl.number_input("Enter the level you want to reach", min_value=start_level+1, max_value=level_cap , value=level_cap )
 
+cote = st.number_input(f"Play with :blue[my cote] and see how Price goes (may help for negociation)", min_value=1 , value=286 )
 nb_level = end_level - start_level
 
 st.write(f"So you wanna do :blue[{nb_level}] levels ?")
@@ -47,6 +47,7 @@ st.dataframe(df,hide_index=True,use_container_width=True)
 st.write(f"So in Sum you need 🌟:blue[{df["🌟Exp Required"].sum():,}] EXP")
 st.write(f"And it will cost 💲:green[{df["💲Prices (Spina)"].sum():,}]")
 
+col2.metric("My cote", f"⚖️{cote}",label_visibility="visible", help=f"A unit to calculate :green[price] base on :blue[exp] needed, More it :red[increase] more the :green[price] is :red[high].\nIt will :red[increase] with game :red[inflation]")
 col3.metric("Total Coast",f"💲{df["💲Prices (Spina)"].sum():,}",label_visibility="visible",help=f"The total amount of :green[money] to level up from :blue[level {start_level}] to :blue[level {end_level}]")
 
 st.write("💡If you ok with or wanna negociate further, just DM me in Discord 🤝")

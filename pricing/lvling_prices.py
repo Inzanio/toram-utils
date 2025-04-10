5aeafc8537f748d6c63d77568292d4160e77fdfe
LoonerDrive <noa.edouardmichel@gmail.com>
2025-04-10 18:08:20 +0100

    change pricing formula

--- pricing/lvling_prices.py
+++ pricing/lvling_prices.py
@@ -31,8 +31,8 @@ def exp_require_for_next_lvl(current_lvl):
 
 def pricing(level):
     price_from_exp = exp_require_for_next_lvl(level)/1000
-    price_from_exp = price_from_exp * 15
-    return price_ceil(level * 1300 + price_from_exp )
+    price_from_exp = price_from_exp * 12
+    return price_ceil(level * 1800 + price_from_exp )
 
 
 import pandas as pd
